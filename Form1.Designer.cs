6833e5b4e349cbe9410e33c28472afe4f49da5ad
Hieu <tranhieu071103@gmail.com>
2024-05-27 16:06:40 +0700

    FormLogin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,105 +31,88 @@ namespace WinFormsAppFinal
         {
             FirstNum = new Label();
             LastNum = new Label();
-            Space = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            btnKq = new Button();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // FirstNum
             // 
-            FirstNum.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             FirstNum.AutoSize = true;
             FirstNum.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold);
-            FirstNum.Location = new Point(216, 83);
+            FirstNum.Location = new Point(234, 128);
             FirstNum.Margin = new Padding(4, 0, 4, 0);
             FirstNum.Name = "FirstNum";
-            FirstNum.Size = new Size(97, 20);
+            FirstNum.Size = new Size(90, 20);
             FirstNum.TabIndex = 0;
-            FirstNum.Text = "So dau tien";
+            FirstNum.Text = "Tai Khoan";
             FirstNum.Click += FirstNum_Click;
             // 
             // LastNum
             // 
-            LastNum.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             LastNum.AutoSize = true;
             LastNum.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold);
-            LastNum.Location = new Point(216, 146);
+            LastNum.Location = new Point(235, 191);
             LastNum.Margin = new Padding(4, 0, 4, 0);
             LastNum.Name = "LastNum";
-            LastNum.Size = new Size(143, 20);
+            LastNum.Size = new Size(89, 20);
             LastNum.TabIndex = 1;
-            LastNum.Text = "So dau cuoi cung";
+            LastNum.Text = "Mat Khau";
             LastNum.Click += LastNum_Click;
-            // 
-            // Space
-            // 
-            Space.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            Space.AutoSize = true;
-            Space.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold);
-            Space.Location = new Point(216, 210);
-            Space.Margin = new Padding(4, 0, 4, 0);
-            Space.Name = "Space";
-            Space.Size = new Size(107, 20);
-            Space.TabIndex = 2;
-            Space.Text = "khoang cach";
-            Space.Click += Space_Click;
             // 
             // textBox1
             // 
-            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBox1.Location = new Point(361, 75);
+            textBox1.Location = new Point(370, 120);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(205, 28);
+            textBox1.Size = new Size(224, 28);
             textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBox2.Location = new Point(361, 138);
+            textBox2.Location = new Point(370, 183);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(205, 28);
+            textBox2.Size = new Size(224, 28);
             textBox2.TabIndex = 4;
+            textBox2.UseSystemPasswordChar = true;
             // 
-            // textBox3
+            // button1
             // 
-            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            textBox3.Location = new Point(361, 202);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(205, 28);
-            textBox3.TabIndex = 5;
+            button1.Location = new Point(234, 273);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 40);
+            button1.TabIndex = 5;
+            button1.Text = "Dang Nhap";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
-            // btnKq
+            // button2
             // 
-            btnKq.AccessibleRole = AccessibleRole.None;
-            btnKq.BackColor = SystemColors.ActiveCaption;
-            btnKq.Location = new Point(361, 284);
-            btnKq.Name = "btnKq";
-            btnKq.Size = new Size(94, 35);
-            btnKq.TabIndex = 6;
-            btnKq.Text = "Ket Qua";
-            btnKq.UseVisualStyleBackColor = false;
-            btnKq.Click += btnKq_Click;
+            button2.Location = new Point(445, 273);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 40);
+            button2.TabIndex = 6;
+            button2.Text = "Thoat";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonFace;
-            ClientSize = new Size(801, 424);
-            Controls.Add(btnKq);
-            Controls.Add(textBox3);
+            ClientSize = new Size(769, 428);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(Space);
             Controls.Add(LastNum);
             Controls.Add(FirstNum);
             Font = new Font("Times New Roman", 10.8F, FontStyle.Bold);
             Margin = new Padding(4);
             Name = "Form1";
-            Text = "TongDaySo";
+            Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -149,10 +132,9 @@ namespace WinFormsAppFinal
 
         private Label FirstNum;
         private Label LastNum;
-        private Label Space;
         private TextBox textBox1;
         private TextBox textBox2;
-        private TextBox textBox3;
-        private Button btnKq;
+        private Button button1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,20 @@ namespace WinFormsAppFinal
 
         }
 
-        private void btnKq_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tinh_S f = new Tinh_S();
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
